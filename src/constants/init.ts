0bc44bb68b0f9b86ec9e6353f5432de3680f0faa
Vik63 <sfoto116@yandex.ru>
2025-02-27 18:09:46 +0400

    add m-auto

--- src/constants/init.ts
+++ src/constants/init.ts
@@ -4,7 +4,7 @@ export const CANVAS_WIDTH_MOBILE = 475;
 export const CANVAS_HEIGHT_MOBILE = 400;
 export const BLOCK_WIDTH = 50;
 export const BLOCK_HEIGHT = 50;
-export const BLOCK_WIDTH_MOBILE = 25;
-export const BLOCK_HEIGHT_MOBILE = 25;
+export const BLOCK_WIDTH_MOBILE = 20;
+export const BLOCK_HEIGHT_MOBILE = 20;
 
 export const directions = ['UP', 'DOWN', 'LEFT', 'RIGHT'];
