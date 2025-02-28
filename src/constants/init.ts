16f47df7c4c1c3b79c542e04150a7a0f30d0b3fc
Vik63 <sfoto116@yandex.ru>
2025-02-28 12:40:53 +0400

    vercel screen_5

--- src/constants/init.ts
+++ src/constants/init.ts
@@ -1,7 +1,7 @@
 export const CANVAS_WIDTH = 950;
 export const CANVAS_HEIGHT = 800;
 export const CANVAS_WIDTH_MOBILE = 475;
-export const CANVAS_HEIGHT_MOBILE = 375;
+export const CANVAS_HEIGHT_MOBILE = 350;
 export const BLOCK_WIDTH = 50;
 export const BLOCK_HEIGHT = 50;
 export const BLOCK_WIDTH_MOBILE = 20;
