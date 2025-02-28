a2e03540e638d3ca0f087c52ed8ad6b02356d89f
Vik63 <sfoto116@yandex.ru>
2025-02-28 20:36:58 +0400

    add comment

--- src/components/Shooting/Shoot.ts
+++ src/components/Shooting/Shoot.ts
@@ -64,7 +64,7 @@ export class Shoot {
    }
 
    render() {
-      // изменяющийся огонь
+      // изменяющийся огонь с мобильным
       let link = this.shoot.fireLink[2];
 
       if (!this.isMobile) {
